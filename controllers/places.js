b51302b6744e29cc739f698da1fe65ae2c25948e
Joseph Graves <jcg97@Josephs-MacBook-Pro.local>
2022-06-15 18:21:12 -0400

    Adding remaining stub routes

--- controllers/places.js
+++ controllers/places.js
@@ -3,5 +3,6 @@ const router = require('express').Router()
 // More code here in a moment
 router.get('/', (req, res) => {
     res.send('GET /places')
-}
-module.exports = router
+})
+
+module.exports=router
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-    res.status('<h1>404 Page</h1>')
+    res.status(404) .send('<h1>404 Page</h1>')
     
 })
 
