269718c1ff4229719d504013f7650f25cca93c6f
Joseph Graves <jcg97@Josephs-MacBook-Pro.local>
2022-06-15 14:49:08 -0400

    Adding 404 pag route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //require modules
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -6,4 +7,15 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+app.get('*', (req, res) => {
+    res.status('<h1>404 Page</h1>')
+    
+})
+
+app.listen(process.env.PORT)
+
+
+
+
+
+
